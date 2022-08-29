59d93a0b9e3a13122162d552285d9fc3bebf5bbd
Michael Zhao <michaelzhaojq@hotmail.com>
2022-08-29 13:25:53 +1200

    Add compiler

--- compile.js
+++ compile.js
@@ -1,1 +1,11 @@
 // compile code will go here
+// Path: For cross platform compatibility, always generate a valid path
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
+
+//__direname > root of this project
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
+const source = fs.readFileSync(inboxPath, 'utf8')
+
+solc.compile(source, 1)
--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -2,14 +2,14 @@ pragma solidity ^0.4.17;
 
 contract Inbox {
     
-    string public message;
+  string public message;
 
-    constructor (string initialMessage) public {
-        message = initialMessage;
-    }
+  constructor (string initialMessage) public {
+    message = initialMessage;
+  }
 
-    function setMessage(string newMessage) public {
-        message = newMessage;
-    }
+  function setMessage(string newMessage) public {
+      message = newMessage;
+  }
 
 }

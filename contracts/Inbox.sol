6dac3460e7c7096b03f62f90dbdb0ec4524696ce
Michael Zhao <michaelzhaojq@hotmail.com>
2022-09-17 22:45:38 +1200

    Organise inbox file

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,15 +1,11 @@
 pragma solidity ^0.4.17;
 
 contract Inbox {
-    
   string public message;
-
-  constructor (string initialMessage) public {
+  function Inbox (string initialMessage) public {
     message = initialMessage;
   }
-
   function setMessage(string newMessage) public {
-      message = newMessage;
+    message = newMessage;
   }
-
 }
